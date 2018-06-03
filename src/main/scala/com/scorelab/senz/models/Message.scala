fe3e37fc0a226c72efb102472e9a1a6ec9f5055f
Sumedhe Dissanayake <sumedhe5dms@gmail.com>
2018-06-04 02:27:22 +0530

    Create Message model

---
+++ src/main/scala/com/scorelab/senz/models/Message.scala
@@ -0,0 +1,27 @@
+package com.scorelab.senz.models
+
+import com.scorelab.senz.models.MessageType.MessageType
+
+/** Enum; for Message Types  */
+object MessageType extends Enumeration {
+  type MessageType = Value
+  val SHARE, UNSHARE, GET, PUT, DATA = Value
+}
+
+/** Structure of the message
+  *
+  * @param messageType the type of the message
+  * @param attributes the attributes
+  * @param sender the sender of the message
+  * @param receiver the receiver of the message
+  * @param timestamp the timestamp the message is sent
+  * @param signature the digital signature of the sender
+  */
+case class Message(
+                    messageType: MessageType,
+                    attributes: Map[String, String],
+                    sender: String,
+                    receiver: String,
+                    timestamp: String = "",
+                    signature: Option[String] = None)
+
