d79ac44aa4449be823bcbfa8a7e816d49c402f56
Sumedhe Dissanayake <sumedhe5dms@gmail.com>
2018-06-11 23:55:07 +0530

    Setup configurations

---
+++ src/main/scala/com/scorelab/senz/config/Config.scala
@@ -0,0 +1,11 @@
+package com.scorelab.senz.config
+
+import com.typesafe.config._
+
+// App configuration
+object Config {
+  val environment: String = if (System.getenv("SCALA_ENV") == null) "development" else System.getenv("SCALA_ENV")
+
+  val config: Config = ConfigFactory.load()
+  def apply(): Config = config.getConfig(environment)
+}
--- src/test/scala/com/scorelab/senz/utils/RSAUtilsTest.scala
+++ src/test/scala/com/scorelab/senz/utils/RSAUtilsTest.scala
@@ -2,12 +2,14 @@ package com.scorelab.senz.utils
 
 import java.security.KeyPair
 
+import com.scorelab.senz.config.Config
 import org.scalatest.FunSuite
 
 class RSAUtilsTest extends FunSuite {
 
   test("Test KeyPair Generation") {
-    val keyPair: KeyPair = RSAUtils.generateKeyPair(1024)
+    val keySize: Int = Config().getInt("app.rsaKeySize")
+    val keyPair: KeyPair = RSAUtils.generateKeyPair(keySize)
     val privateKey: String = RSAUtils.privateKeyToString(keyPair.getPrivate)
     val publicKey: String = RSAUtils.publicKeyToString(keyPair.getPublic)
     println(s"Private Key: $privateKey")
