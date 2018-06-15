7d0e1fa5cd4c5632cd4252df9bfc346ba7a6d49d
Sumedhe Dissanayake <sumedhedissanayake@gmail.com>
2018-06-15 19:37:50 +0530

    Modify DB Connection

--- src/main/scala/com/scorelab/senz/database/MongoFactory.scala
+++ src/main/scala/com/scorelab/senz/database/MongoFactory.scala
@@ -4,13 +4,16 @@ import com.mongodb.casbah.{MongoClient, MongoCollection, MongoConnection}
 import com.scorelab.senz.config.AppConfig
 
 object MongoFactory {
- //  DB Connection
+  //  DB Configs
   val SERVER: String = AppConfig().getString("db.mongo.host")
   val PORT: Int = AppConfig().getInt("db.mongo.port")
   val DATABASE: String = AppConfig().getString("db.mongo.database")
 
- //  DB Collections
-  val connection = MongoConnection(SERVER)
-  val senzCollection: MongoCollection = connection(DATABASE)("senz")
-  val clientsCollection: MongoCollection = connection(DATABASE)("clients")
+  //  DB Connection
+  val mongoClient = MongoClient(SERVER)
+  val db = mongoClient(DATABASE)
+
+  // DB Collections
+  val clientsCollection = db("clients")
+  val senzCollection = db("senz")
 }
--- src/test/scala/com/scorelab/senz/database/DatabaseTest.scala
+++ src/test/scala/com/scorelab/senz/database/DatabaseTest.scala
@@ -6,6 +6,20 @@ import org.scalatest.FunSuite
 class DatabaseTest extends FunSuite {
 
   test("testConnection") {
-  
+    val coll = MongoFactory.db("test")
+    coll.drop
+
+    // Insert new object
+    val builder = MongoDBObject.newBuilder
+    builder += "name" -> "db test"
+    val result = coll.insert(builder.result)
+
+    // Check for the object
+    var items = coll.find
+    assert(items.count === 1)
+
+    // Drop the collection
+    coll.drop
+
   }
 }
