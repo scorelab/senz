ab510a1e5996548cf6ee54a5d9d508d852e47b67
Sumedhe Dissanayake <sumedhedissanayake@gmail.com>
2018-07-05 08:44:18 +0530

    Complete message parsing between two devices

--- src/main/scala/com/scorelab/senz/Main.scala
+++ src/main/scala/com/scorelab/senz/Main.scala
@@ -1,6 +1,9 @@
 package com.scorelab.senz
 
+import com.scorelab.senz.utils.MessageUtils
+
 object Main extends App{
   println("Initalizing SenZ..")
-
+  val query = "GET #pubkey @device2 #time time1 ^device1 Signature"
+  val message = MessageUtils.parseMessage(query)
 }
--- src/main/scala/com/scorelab/senz/socket/SocketHandlerActor.scala
+++ src/main/scala/com/scorelab/senz/socket/SocketHandlerActor.scala
@@ -1,7 +1,10 @@
 package com.scorelab.senz.socket
 
-import akka.actor.{Actor, ActorLogging, ActorRef}
+import akka.actor.{Actor, ActorLogging, ActorRef, Props}
 import akka.io.Tcp
+import akka.util.ByteString
+import com.scorelab.senz.models.MessageType
+import com.scorelab.senz.utils.MessageUtils
 
 //Senz Socket handler
 class SocketHandlerActor(device: ActorRef) extends Actor with ActorLogging {
@@ -10,6 +13,24 @@ class SocketHandlerActor(device: ActorRef) extends Actor with ActorLogging {
   def receive = {
     case Tcp.Received(data) =>
       print(data.utf8String)
+      val query: String = data.utf8String
+      val message = MessageUtils.parseMessage(query)
+
+      // Login device
+      if (message.messageType == MessageType.SHARE && query.contains("pubkey")){ // TODO: Check pubkey from the atttributes
+        SessionManager.login(message.sender, device); // Add to the session
+        device ! Tcp.Write(ByteString("Device registered\n"))
+        println(device)
+      } else if (message.messageType == MessageType.DATA){ // Send message
+        if (SessionManager.sessions.contains(message.receiver)){
+          val receiverActor: ActorRef = SessionManager.getSession(message.receiver).value
+          receiverActor ! Tcp.Write(ByteString("Message Received from " + message.sender + ": " + data.utf8String))
+          device ! Tcp.Write(ByteString("Message is sent to the device " + message.receiver + "\n"))
+        } else {
+          device ! Tcp.Write(ByteString("Device is not logged in\n"))
+        }
+      }
+
    case Tcp.PeerClosed =>
       log.info("Device disconnected")
       context stop self
