bad754c338cac6fba7cd4c22e92c92c0b61adff9
Sumedhe Dissanayake <sumedhedissanayake@gmail.com>
2018-08-06 19:45:43 +0530

    Setup Travis-CI

--- src/test/scala/com/scorelab/senz/socket/SocketTest.scala
+++ src/test/scala/com/scorelab/senz/socket/SocketTest.scala
@@ -10,6 +10,6 @@ class SocketTest extends FunSuite {
     val server = system.actorOf(Props[SocketListenerActor], name = "server")
 
     // Keep the socket alive
-    Thread.sleep(5000000)
+    Thread.sleep(1000)
   }
 }
