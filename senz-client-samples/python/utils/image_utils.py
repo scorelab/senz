322ccb8c5a2dfc1e2d5bbcf3e9d832f42ba51dfd
YashMeh <mehrotra.yash9@gmail.com>
2019-02-20 00:33:21 +0530

    Updated imageUtility

--- senz-client-samples/python/utils/image_utils.py
+++ senz-client-samples/python/utils/image_utils.py
@@ -3,13 +3,11 @@ import base64
 # Convert image to string
 def imageToString(fileName):
 	with open(fileName, "rb") as imageFile:
-		str = base64.b64encode(imageFile.read())
-		#print str
-		return str
-
+		return base64.b64encode(imageFile.read())
+		
 # Convert string to image
 def stringToImage(str, fileName):
 	fh = open(fileName, "wb")
-	fh.write(str.decode('base64'))
+	fh.write(base64.b64decode(str.encode()))
 	fh.close()
 
