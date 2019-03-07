9dfcbced7d5138478755f8f72c68e0dbdb44efb2
Pradip.Karnavat <pradip.karnavat@administrators-MacBook-Air-4.local>
2019-03-07 12:09:58 +0530

    resolved no console and arrow function check issue in serviceWorker

--- admin-panel/src/serviceWorker.js
+++ admin-panel/src/serviceWorker.js
@@ -20,35 +20,6 @@ const isLocalhost = Boolean(
   )
 );
 
-function checkValidServiceWorker(swUrl, config) {
-  // Check if the service worker can be found. If it can't reload the page.
-  fetch(swUrl)
-    .then((response) => {
-      // Ensure service worker exists, and that we really are getting a JS file.
-      const contentType = response.headers.get("content-type");
-      if (
-        response.status === 404 ||
-        (contentType != null && contentType.indexOf("javascript") === -1)
-      ) {
-        // No service worker found. Probably a different app. Reload the page.
-        navigator.serviceWorker.ready.then(registration => {
-          registration.unregister().then(() => {
-            window.location.reload();
-          });
-        });
-      } else {
-        // Service worker found. Proceed as normal.
-        registerValidSW(swUrl, config);
-      }
-    })
-    .catch(() => {
-      // console.log(
-      //   "No internet connection found. App is running in offline mode."
-      // );
-    });
-}
-
-
 function registerValidSW(swUrl, config) {
   navigator.serviceWorker
     .register(swUrl)
@@ -95,6 +66,36 @@ function registerValidSW(swUrl, config) {
 }
 
 
+function checkValidServiceWorker(swUrl, config) {
+  // Check if the service worker can be found. If it can't reload the page.
+  fetch(swUrl)
+    .then((response) => {
+      // Ensure service worker exists, and that we really are getting a JS file.
+      const contentType = response.headers.get("content-type");
+      if (
+        response.status === 404 ||
+        (contentType != null && contentType.indexOf("javascript") === -1)
+      ) {
+        // No service worker found. Probably a different app. Reload the page.
+        navigator.serviceWorker.ready.then((registration) => {
+          registration.unregister().then(() => {
+            window.location.reload();
+          });
+        });
+      } else {
+        // Service worker found. Proceed as normal.
+        registerValidSW(swUrl, config);
+      }
+    })
+    .catch(() => {
+      // console.log(
+      //   "No internet connection found. App is running in offline mode."
+      // );
+    });
+}
+
+
+
 export function register(config) {
   if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator) {
     // The URL constructor is available in all browsers that support SW.
@@ -116,10 +117,10 @@ export function register(config) {
         // Add some additional logging to localhost, pointing developers to the
         // service worker/PWA documentation.
         navigator.serviceWorker.ready.then(() => {
-          console.log(
-            "This web app is being served cache-first by a service " +
-            "worker. To learn more, visit http://bit.ly/CRA-PWA"
-          );
+          // console.log(
+          //   "This web app is being served cache-first by a service " +
+          //   "worker. To learn more, visit http://bit.ly/CRA-PWA"
+          // );
         });
       } else {
         // Is not localhost. Just register service worker
