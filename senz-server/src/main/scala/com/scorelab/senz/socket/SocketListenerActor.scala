68bf61d5abf603cc0e3fe1e3b422182286263819
Sumedhe Dissanayake <sumedhedissanayake@gmail.com>
2019-03-11 03:41:07 +0530

    Fix context issue

--- senz-server/src/main/scala/com/scorelab/senz/socket/SocketListenerActor.scala
+++ senz-server/src/main/scala/com/scorelab/senz/socket/SocketListenerActor.scala
@@ -1,7 +1,6 @@
 package com.scorelab.senz.socket
 
 import java.net.InetSocketAddress
-import context.system
 
 import akka.actor._
 import akka.io.{IO, Tcp}
@@ -9,6 +8,7 @@ import com.scorelab.senz.config.AppConfig
 
 //Senz Socket
 class SocketListenerActor extends Actor with ActorLogging {
+  import context.system
 
   val hostname : String = AppConfig().getString("app.socket.hostname")
   val port : Int = AppConfig().getInt("app.socket.port")
