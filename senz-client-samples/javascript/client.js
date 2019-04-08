c188512d37452ea883b69b8a04d2e854e9293d0d
Subash Aravindan <p.subasharavindan@gmail.com>
2019-04-08 21:00:53 +0530

    Fix logging in JS client to avoid console statements

--- senz-client-samples/javascript/client.js
+++ senz-client-samples/javascript/client.js
@@ -1,12 +1,13 @@
 const net = require("net");
+const logger = require('./utils/winstonConfig')
 // Create a socket (client) that connects to the server
 var socket = new net.Socket();
 socket.connect(2552, "localhost", function () {
-    console.log("Client: Connected to server");
+    logger.info("Client: Connected to server");
 });
 
 var sendMessage=function(msg){
-    return new Promise(function(resolve,reject){
+    return new Promise(function(resolve, reject){
         socket.write(msg);
         socket.on("data",function(data){
         data=data.toString('utf8')
@@ -15,21 +16,21 @@ var sendMessage=function(msg){
 
     })
       socket.on('error', function(e){
-        console.log("Error Message:",e);
+        logger.error(`Error Message: ${e}`);
    })
 
     })
 
 }
 var getResponse=function(){
-    return new Promise(function(resolve,reject){
+    return new Promise(function(resolve, reject){
             socket.on("data",function(data){
                 data=data.toString('utf8').split(" ")[2]
                 var chunk="";
                 for(var i=0;i<data.length;i++){
                     chunk+=data[i];
                 }
-                console.log(chunk);
+                logger.debug(chunk);
                 resolve(chunk);
             })
 
