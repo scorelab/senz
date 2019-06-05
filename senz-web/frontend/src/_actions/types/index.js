9ef1ea680f28cbda6bba83cffef4125181f903ce
YashMeh <mehrotra.yash9@gmail.com>
2019-06-06 02:07:43 +0530

    Added delete reducer

--- senz-web/frontend/src/_actions/types/index.js
+++ senz-web/frontend/src/_actions/types/index.js
@@ -5,3 +5,4 @@ export const AUTHENTICATION_ERROR = "authentication_error";
 export const ADD_PROJECT = "add_project";
 export const FETCH_PROJECTS = "fetch_projects";
 export const SET_PROJECT = "set_project";
+export const DELETE_PROJECT = "delete_project";
