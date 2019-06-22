66a4aa5065e48486b675effc7bec3860c9d69d56
YashMeh <mehrotra.yash9@gmail.com>
2019-06-22 16:38:59 +0530

    Add integration tests

---
+++ senz-web/frontend/src/_integrationTests/storageMock.js
@@ -0,0 +1,17 @@
+export function storageMock() {
+  let store = {};
+  return {
+    getItem: function(key) {
+      return store[key] || null;
+    },
+    setItem: function(key, value) {
+      store[key] = value.toString();
+    },
+    removeItem: function(key) {
+      delete store[key];
+    },
+    clear: function() {
+      store = {};
+    }
+  };
+}
